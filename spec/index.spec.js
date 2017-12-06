420a56d08771eb6f9376b8dc6bb60f9bbaacd941
Ajinkya Borade <ajinkyaborade@Ajinkyas-MacBook-Pro.local>
2017-12-06 17:37:35 +0530

    basic spec

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,4 +1,13 @@
-var Jasmine = require('jasmine');
-var jasmine = new Jasmine();
-jasmine.loadConfigFile('spec/support/jasmine.json');
 
+describe('updateStocks', function _stocksHelper() {
+  var stocks, sparks;
+  beforeEach(function() {
+    stocks = new Map();
+    sparks = new Map();
+  });
+
+  it("stocks should new empty map", function() {
+    expect(typeof stocks.entries()).toBe('object');   
+  });
+
+});
